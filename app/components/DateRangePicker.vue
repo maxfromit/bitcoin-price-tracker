dc2412fd8af8deb5d835720bd478399839b94029
maximfromit <bigmaxcapacity@gmail.com>
2025-06-20 23:16:19 +0300

    components/DateRangePicker: add DateRangePicker component for selecting date ranges components/PeriodPicker: add PeriodPicker component for selecting periods

---
+++ app/components/DateRangePicker.vue
@@ -0,0 +1,137 @@
+<script setup lang="ts">
+import type { DateValue } from "@internationalized/date"
+
+import {
+  CalendarDate,
+  DateFormatter,
+  getLocalTimeZone,
+  fromAbsolute,
+  toCalendarDate,
+} from "@internationalized/date"
+
+const props = defineProps<{
+  firstDate: DateValue
+  lastDate: DateValue
+}>()
+
+type DateRange = {
+  start: DateValue | null
+  end: DateValue | null
+}
+
+const selectedRange = defineModel<DateRange>("selectedRange")
+
+const df = new DateFormatter("en-US", {
+  dateStyle: "medium",
+})
+
+const maxDate = computed(() => {
+  return props.lastDate?.copy() ?? undefined
+})
+
+const minDate = computed(() => {
+  return props.firstDate?.copy() ?? undefined
+})
+
+watch(
+  () => selectedRange.value?.start,
+  (newStart) => {
+    if (
+      newStart &&
+      selectedRange.value?.end &&
+      newStart.compare(selectedRange.value.end.copy()) > 0
+    ) {
+      selectedRange.value.end = newStart.copy()
+    }
+  }
+)
+
+watch(
+  () => selectedRange.value?.end,
+  (newEnd) => {
+    if (
+      newEnd &&
+      selectedRange.value?.start &&
+      newEnd.compare(selectedRange.value.start.copy()) < 0
+    ) {
+      selectedRange.value.start = newEnd.copy()
+    }
+  }
+)
+
+function resetSelectedRange() {
+  selectedRange.value = {
+    start: props.firstDate?.copy(),
+    end: props.lastDate?.copy(),
+  }
+}
+
+const isNotDefaultRange = computed(() => {
+  if (
+    !selectedRange.value ||
+    !selectedRange.value.start ||
+    !selectedRange.value.end
+  ) {
+    return false
+  }
+  return (
+    selectedRange.value.start?.compare(props.firstDate?.copy()) !== 0 ||
+    selectedRange.value.end?.compare(props.lastDate?.copy()) !== 0
+  )
+})
+</script>
+
+<template>
+  <template v-if="selectedRange">
+    <UPopover>
+      <UButton color="neutral" variant="subtle" icon="i-lucide-calendar">
+        {{
+          selectedRange.start
+            ? df.format(selectedRange.start.toDate(getLocalTimeZone()))
+            : "Select a date"
+        }}
+      </UButton>
+
+      <template #content>
+        <UCalendar
+          v-model="selectedRange.start"
+          class="p-2"
+          :min-value="minDate"
+          :max-value="maxDate"
+        />
+      </template>
+    </UPopover>
+
+    <UPopover>
+      <UButton color="neutral" variant="subtle" icon="i-lucide-calendar">
+        {{
+          selectedRange.end
+            ? df.format(selectedRange.end.toDate(getLocalTimeZone()))
+            : "Select a date"
+        }}
+      </UButton>
+
+      <template #content>
+        <UCalendar
+          v-model="selectedRange.end"
+          :min-value="minDate"
+          :max-value="maxDate"
+          class="p-2"
+        />
+      </template>
+    </UPopover>
+
+    <UButton
+      v-if="
+        selectedRange &&
+        selectedRange.start &&
+        selectedRange.end &&
+        isNotDefaultRange
+      "
+      icon="i-lucide-x"
+      color="primary"
+      variant="solid"
+      @click="resetSelectedRange()"
+    />
+  </template>
+</template>
---
+++ app/components/PeriodPicker.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Period } from "./BitcoinPriceChart/BitcoinPriceChart.vue"
+import type { RadioGroupItem } from "@nuxt/ui"
+
+const selectedPeriod = defineModel<Period>("selectedPeriod")
+const periods = defineModel<RadioGroupItem[]>("periods")
+</script>
+
+<template>
+  <URadioGroup
+    v-if="periods && periods.length > 0"
+    v-model="selectedPeriod"
+    orientation="horizontal"
+    variant="list"
+    default-value="all"
+    :items="periods"
+  />
+</template>
