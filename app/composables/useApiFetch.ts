db886dcc7eb942b6ba53613b80e9eb4daf6dff08
maximfromit <bigmaxcapacity@gmail.com>
2025-06-20 23:31:31 +0300

    app/composables/useApiFetch: remove unused useApiFetch composable

--- app/composables/useApiFetch.ts
+++
@@ -1,9 +0,0 @@
-// import type { UseFetchOptions } from "#app"
-
-// export function useApiFetch<T>(path: string, options?: UseFetchOptions<T>) {
-//   const config = useRuntimeConfig()
-//   return useFetch(path, {
-//     baseURL: config.public.apiBase,
-//     ...options,
-//   })
-// }
