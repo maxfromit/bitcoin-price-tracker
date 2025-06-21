78dd76972a965974eab68578895988d1332dd982
maximfromit <bigmaxcapacity@gmail.com>
2025-06-21 17:55:25 +0300

    server/api/bitcoin-prices update variable name for clarity

--- server/api/bitcoin-prices.ts
+++ server/api/bitcoin-prices.ts
@@ -2,9 +2,9 @@ import { db } from "~~/server/utils/db"
 import { bitcoinPrice } from "~~/server/db/schema"
 
 export default defineEventHandler(async () => {
-  const prices = await db
+  const bitcoinPrices = await db
     .select()
     .from(bitcoinPrice)
     .orderBy(bitcoinPrice.evaluatedAt)
-  return prices
+  return bitcoinPrices
 })
