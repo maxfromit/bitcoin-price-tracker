1d0dfd43e1850bca48a6410d69e6c73545228b99
maximfromit <bigmaxcapacity@gmail.com>
2025-06-21 17:56:31 +0300

    server/utils/fetchHistoricalPrices update function to improve data fetching logic and error handling

--- server/utils/fetchHistoricalPrices.ts
+++ server/utils/fetchHistoricalPrices.ts
@@ -4,62 +4,74 @@ import type {
 } from "~~/server/types"
 
 const baseUrl = "https://data-api.coindesk.com/index/cc/v1/historical/days"
-// const params = {
-//   market: "cadli",
-//   instrument: "BTC-USD",
-//   limit: 1,
-//   aggregate: 1,
-//   fill: "true",
-//   apply_mapping: "true",
-//   response_format: "JSON",
-// }
+const DAY_SECONDS = 86400
 
-export async function fetchHistoricalPrices(
-  limit?: string
-): Promise<HistoricalPriceItem[]> {
+export async function fetchHistoricalPrices(dataFetchLimit?: number) {
+  const allData: HistoricalPriceItem[] = []
+
+  let toTs: number | undefined
+  let keepFetching = true
+  let limit = 2000
   const url = new URL(baseUrl)
-
-  const params = {
-    market: "cadli",
-    instrument: "BTC-USD",
-    limit: limit ? limit : "2",
-    aggregate: "1",
-    fill: "true",
-    apply_mapping: "true",
-    response_format: "JSON",
-  }
-
-  url.search = new URLSearchParams(params).toString()
 
   const options = {
     method: "GET",
     headers: { "Content-type": "application/json; charset=UTF-8" },
   }
-  // console.log("Fetching historical prices from url.toString():", url.toString())
-  // console.log("Fetching historical prices from url:", url)
-  // console.log("params:", params)
 
-  try {
-    const response = await fetch(url.toString(), options)
-
-    if (!response.ok) {
-      const errorResponse = await response.json()
-      throw {
-        status: response.status,
-        statusText: response.statusText,
-        details: errorResponse,
-      }
+  while (keepFetching) {
+    // if dataFetchLimit is set, adjust the limit accordingly: it should not exceed the remaining data to fetch
+    if (dataFetchLimit) {
+      limit = Math.min(limit, dataFetchLimit - allData.length)
     }
 
-    // console.log("Response status:", response)
-    const res = (await response.json()) as HistoricalPriceResponse
+    const params = {
+      market: "cadli",
+      instrument: "BTC-USD",
+      limit: String(limit),
+      aggregate: "1",
+      fill: "true",
+      apply_mapping: "true",
+      response_format: "JSON",
+      ...(toTs ? { to_ts: String(toTs) } : {}),
+    }
 
-    console.log("Count of fetched rows", res.Data.length) // Log the fetched data
-    console.log("Example of Data", res.Data) // Log the fetched data
+    url.search = new URLSearchParams(params).toString()
 
-    return res.Data
-  } catch (error) {
-    console.error("Error fetching historical prices")
-    throw error // Re-throw the error to let the caller handle it
+    try {
+      const response = await fetch(url.toString(), options)
+
+      if (!response.ok) {
+        const errorResponse = await response.json()
+        throw {
+          status: response.status,
+          statusText: response.statusText,
+          details: errorResponse,
+        }
+      }
+
+      const res = (await response.json()) as HistoricalPriceResponse
+      const batch = res.Data
+      if (!batch || !Array.isArray(batch) || !batch[0]?.TIMESTAMP) {
+        console.warn(
+          "No data found in the response or data is not an array or TIMESTAMP is missing"
+        )
+        keepFetching = false
+        break
+      }
+
+      allData.push(...batch)
+      keepFetching =
+        batch.length >= parseInt(params.limit, 10) &&
+        ((dataFetchLimit && allData.length < dataFetchLimit) || !dataFetchLimit)
+
+      const earliest = batch[0].TIMESTAMP
+      toTs = earliest - DAY_SECONDS // Set to the earliest timestamp minus one day
+    } catch (error) {
+      console.error("Error fetching historical prices")
+      throw error
+    }
   }
+  console.log("fetchHistoricalPrices allData length:", allData.length)
+  return allData
 }
