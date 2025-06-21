f128fb0a0ce88792967a4d72f06be6fa300cf4ce
maximfromit <bigmaxcapacity@gmail.com>
2025-06-21 17:55:40 +0300

    server/plugins/01-populate-bitcoin-price update plugin to include error handling and logging

--- server/plugins/01-populate-bitcoin-price.ts
+++ server/plugins/01-populate-bitcoin-price.ts
@@ -1,5 +1,12 @@
 import { fetchTransformAndInsertBitcoinPrices } from "../utils/fetchTransformAndInsertBitcoinPrices"
 
-export default async () => {
-  await fetchTransformAndInsertBitcoinPrices()
-}
+export default defineNitroPlugin(async () => {
+  try {
+    await fetchTransformAndInsertBitcoinPrices()
+    console.log(
+      "Plugin 01-populate-bitcoin-price: bitcoin prices populated successfully."
+    )
+  } catch (error) {
+    console.error("Failed to populate Bitcoin prices:", error)
+  }
+})
