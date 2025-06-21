ccd06eecd850b81521b18270b2b55c8f199797fb
maximfromit <bigmaxcapacity@gmail.com>
2025-06-21 12:01:06 +0300

    BitcoinPriceChart/types: update DateRange to use CalendarDate instead of DateValue

--- app/components/BitcoinPriceChart/types.ts
+++ app/components/BitcoinPriceChart/types.ts
@@ -1,8 +1,9 @@
-import type { DateValue } from "@internationalized/date"
+// import type { DateValue } from "@internationalized/date"
+import type { CalendarDate } from "@internationalized/date"
 
 export type DateRange = {
-  start: DateValue | null
-  end: DateValue | null
+  start: CalendarDate | null
+  end: CalendarDate | null
 }
 
 export type Period = "1d" | "1w" | "1m" | "1y" | "all" | "custom"
