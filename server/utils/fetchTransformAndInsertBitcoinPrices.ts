49c8a9a600301c272e9ca9f9668cf07afe0dc99d
maximfromit <bigmaxcapacity@gmail.com>
2025-06-22 12:49:16 +0300

    server fetchTransformAndInsertBitcoinPrices refactor to use dataFetchLimit as argument and remove instruction handling

--- server/utils/fetchTransformAndInsertBitcoinPrices.ts
+++ server/utils/fetchTransformAndInsertBitcoinPrices.ts
@@ -2,22 +2,18 @@ import { db } from "./db"
 import { bitcoinPrice } from "../db/schema"
 import { fetchHistoricalPrices } from "./fetchHistoricalPrices"
 import { transformFetchedDataForBitcoinPrice } from "./transformFetchedDataForBitcoinPrice"
+
 export async function fetchTransformAndInsertBitcoinPrices(
-  instruction?: "insertAll" | "insertLatest"
+  dataFetchLimit?: number
 ): Promise<void> {
   try {
     console.log("Starting to fetch, transform, and insert Bitcoin prices...")
-
-    if (instruction === "insertAll") {
-      console.log("deleting all historical prices as per instruction.")
-      await db.delete(bitcoinPrice)
-    }
 
     const countExistedRows = await db.$count(bitcoinPrice)
     console.log("countRows", countExistedRows)
 
     const fetchedData = await fetchHistoricalPrices(
-      instruction === "insertLatest" ? 5 : undefined
+      dataFetchLimit ? dataFetchLimit : undefined
     )
 
     if (fetchedData.length === countExistedRows && countExistedRows > 0) {
@@ -25,49 +21,28 @@ export async function fetchTransformAndInsertBitcoinPrices(
       return
     }
 
-    let latestBitcoinPrice = null
-    if (instruction === "insertLatest") {
-      console.log("Fetching only the latest Bitcoin prices...")
-      latestBitcoinPrice = await db.query.bitcoinPrice.findFirst({
-        orderBy: (table, { desc }) => [desc(table.evaluatedAt)],
-      })
-      console.log("Latest evaluatedAt:", latestBitcoinPrice?.evaluatedAt)
-    }
-
-    const dataToInsert =
-      countExistedRows !== 0 && latestBitcoinPrice
-        ? fetchedData.filter(
-            (item) =>
-              typeof item.TIMESTAMP === "number" &&
-              item.TIMESTAMP > latestBitcoinPrice?.evaluatedAt
-          )
-        : fetchedData
-
-    if (dataToInsert.length > 0) {
-      const transformedData = transformFetchedDataForBitcoinPrice(dataToInsert)
+    if (fetchedData.length > 0) {
+      const transformedData = transformFetchedDataForBitcoinPrice(fetchedData)
 
       console.log(`inserting ${transformedData.length} rows`)
 
-      await db
-        .insert(bitcoinPrice)
-        .values(transformedData)
-        .onConflictDoNothing()
-
-      console.log("Data successfully inserted into bitcoin_price table.")
-
-      // .returning({ insertedDates: bitcoinPrice.evaluatedAt })
-
-      // const insertedDates = await db
+      // await db
       //   .insert(bitcoinPrice)
       //   .values(transformedData)
       //   .onConflictDoNothing()
-      //   .returning({ insertedDates: bitcoinPrice.evaluatedAt })
-      // setLoadingState(null)
 
-      // console.log(
-      //   "Data successfully inserted into bitcoin_price table.",
-      //   insertedDates
-      // )
+      // console.log("Data successfully inserted into bitcoin_price table.")
+
+      const insertedDates = await db
+        .insert(bitcoinPrice)
+        .values(transformedData)
+        .onConflictDoNothing()
+        .returning({ insertedDates: bitcoinPrice.evaluatedAt })
+
+      console.log(
+        "Data successfully inserted into bitcoin_price table.",
+        insertedDates
+      )
     } else {
       console.log("No valid data to insert into bitcoin_price table.")
     }
